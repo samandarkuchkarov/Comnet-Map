94b4e9e4ad4fcaf4fe4d73a98689d425f1a63d64
samandar <samandar8939522@gmail.com>
2021-06-15 10:36:44 +0300

    add

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility"
 import React from 'react'
-import data from '../location.json'
+import data from './location.json'
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 const Map = () => {
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import dynamic from "next/dynamic";
 import React from "react";
 
 export default function Home() {
-  const MapWithNoSSR = dynamic(() => import("./map"), {
-    ssr: false
-  });
+  const DynamicComponentWithNoSSR = dynamic(
+    () => import('../map'),
+    { ssr: false }
+  )
 
   return (
     <div style={{justifyContent:'center',alignItems:'center'}} className="container">
-   <MapWithNoSSR />
+   <DynamicComponentWithNoSSR />
     </div>
   )
 }
